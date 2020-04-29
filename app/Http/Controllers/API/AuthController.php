d28842a899932c9035a27d9d25b283f208b41e63
Salwa M <codernist.s@gmail.com>
2020-04-29 12:59:29 +0500

    Login Api

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Database\QueryException;
 use Validator;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends BaseController
 {
@@ -38,7 +39,7 @@ class AuthController extends BaseController
             $input['password'] = bcrypt($input['password']);
             $input['last_login'] = now();
             $user = User::create($input);
-            $returnData['token'] =  $user->createToken('Artist Register')->accessToken;
+            $returnData['token'] =  $user->createToken('User Register')->accessToken;
             $returnData['username'] =  $user->username;
         
         }catch(QueryException $ex) {
@@ -51,4 +52,40 @@ class AuthController extends BaseController
    
         return $this->sendResponse($returnData, 'User register successfully.');
     }
+
+
+    public function login(Request $request)
+    {
+        $returnData = [];
+
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+        try {
+
+            if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+                $user = Auth::user(); 
+                $success['token'] =  $user->createToken('User Login')-> accessToken; 
+                $success['name'] =  $user->name;
+       
+                return $this->sendResponse($success, 'User login successfully.');
+            } 
+            else{ 
+                return $this->sendError('Invalid Email or Password.', ['error'=>'Unauthorised']);
+            } 
+        
+        }catch(QueryException $ex) {
+            return $this->sendError('Validation Error.', $ex->getMessage(), 200);
+        }catch(Exception $ex) {
+            return $this->sendError('Unknown Error', $ex->getMessage(), 200);       
+        }
+
+    }
+
+
 }
