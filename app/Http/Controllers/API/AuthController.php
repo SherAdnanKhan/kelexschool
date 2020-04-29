9c0bbe723e7812f3d7fa0379c3ec106dc00158e9
Salwa M <codernist.s@gmail.com>
2020-04-29 13:01:16 +0500

    login api sucess change

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -70,10 +70,9 @@ class AuthController extends BaseController
 
             if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
                 $user = Auth::user(); 
-                $success['token'] =  $user->createToken('User Login')-> accessToken; 
-                $success['name'] =  $user->name;
+                $returnData['token'] =  $user->createToken('User Login')-> accessToken; 
        
-                return $this->sendResponse($success, 'User login successfully.');
+                return $this->sendResponse($returnData, 'User login successfully.');
             } 
             else{ 
                 return $this->sendError('Invalid Email or Password.', ['error'=>'Unauthorised']);
