869ebc4957f5a61808cc45ea527f129f58627b92
Salwa M <codernist.s@gmail.com>
2020-05-05 15:33:18 +0500

    Image on register

--- app/Http/Controllers/API/BaseController.php
+++ app/Http/Controllers/API/BaseController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Storage;
 
 class BaseController extends Controller
 {
@@ -21,8 +22,6 @@ class BaseController extends Controller
             'data'    => $result,
             'message' => $message,
         ];
-
-
         return response()->json($response, 200);
     }
 
@@ -44,5 +43,30 @@ class BaseController extends Controller
         }
 
         return response()->json($response, $code);
+    }
+
+    public function uploadImage($file, $directory)
+    {
+        $returnData=[];
+        $fileNameStore = time();
+        $extention = $file->getClientOriginalExtension();
+        $uuid = $this->generateRandomString();
+        $imageName = $uuid.'-'.$fileNameStore.'.'.$extention;
+        $image = $file;
+
+        Storage::disk('s3')->put($directory.$imageName, file_get_contents($image), 'public');
+        $image_path = Storage::disk('s3')->url($directory.$imageName);
+        $returnData = ['image_path' => $image_path, 'image_name' => $imageName];
+        return $returnData;
+    }
+
+    public function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
