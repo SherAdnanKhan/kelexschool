bc752aa5afd7b8bb3c860c9df55d224276d5cfa0
Salwa M <codernist.s@gmail.com>
2020-05-15 12:45:58 +0500

    Fave gallery API

--- app/Http/Controllers/API/v1/GalleryController.php
+++ app/Http/Controllers/API/v1/GalleryController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers\API\v1;
 use App\Http\Controllers\API\v1\BaseController;
 use Illuminate\Http\Request;
 use Auth;
+use Illuminate\Database\QueryException;
+use Validator;
 use App\Models\Gallery;
 use App\Models\Post;
+use App\Models\UserFavGallery;
 
 class GalleryController extends BaseController
 {
@@ -28,5 +31,31 @@ class GalleryController extends BaseController
         $posts = Post::where('gallery_id', $gallery->id)->get();
         return $this->sendResponse($posts, 'Gallery posts');
     }
+
+    public function make_fav(Request $request)
+    {
+        $returnData = [];
+        $user = Auth::guard('api')->user();
+        $validator = Validator::make($request->all(), [
+            'gallery_id' => 'required',
+        ]);
+   
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+        try {
+            $input = $request->all();
+            $input['user_id'] = $user->id;
+            $art = UserFavGallery::create($input);
+
+        }catch(QueryException $ex) {
+            return $this->sendError('Validation Error.', $ex->getMessage(), 200);
+        }catch(Exception $ex) {
+            return $this->sendError('Unknown Error', $ex->getMessage(), 200);       
+        }
+        return $this->sendResponse($returnData, 'Mark into Fave Successfully.');
+
+    }
+
 
 }
