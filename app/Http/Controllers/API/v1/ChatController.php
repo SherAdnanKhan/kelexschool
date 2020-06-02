bb273f36752596b29054dac058f20a832cb2eb43
Salwa M <codernist.s@gmail.com>
2020-06-02 19:25:42 +0500

    update read message

--- app/Http/Controllers/API/v1/ChatController.php
+++ app/Http/Controllers/API/v1/ChatController.php
@@ -74,7 +74,7 @@ class ChatController extends BaseController
             $returnData['conversation'] = $new_conversation;
         }
         else {
-            $messages_logs = MessageLog::where('conversation_id', $hasConversation->id)->where('user_id', $user->id)->update(['status', 1]);
+            $messages_logs = MessageLog::where('conversation_id', $hasConversation->id)->where('user_id', $user->id)->update(['status' => 1]);
             $returnData['conversation'] = $hasConversation;
         }
         return $this->sendResponse($returnData, 'User One to one Conversation');
