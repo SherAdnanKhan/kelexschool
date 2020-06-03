81daf096b8ddcd1377197aed5c935a9c18a6ba6e
Akif <m.akif.farooq@gmail.com>
2020-06-03 22:17:37 +0500

    added and implemented notifications functionality in node

--- socket.js
+++ socket.js
@@ -5,29 +5,34 @@ const io = require('socket.io')(http);
 
 io.sockets.on('connection', function (socket) {
   console.log("user connected");
+
   socket.on('join', (data, callback) => {
     socket.join(data.room);
     console.log("room joined");
     console.log(data.room);
     callback && callback();
   });
+
+  socket.on('joinNotification', (room, callback) => {
+    socket.join(room);
+    callback && callback();
+  });
+
   socket.on('sendMessage', (data, callback) => {
-    //io.sockets.in(data.room).emit('recieveMessage', data);
-
     io.to(data.room).emit('recieveMessage', data);
-    //socket.emit('notification',{ description: 'You have a new Message'});
-    socket.broadcast.emit('notification', { description: data.room + ' Have a new message' });
+    io.to('notify').emit('notify', data);
 
-    // io.sockets.in(message.room).emit('message', message.message);
     console.log("message", data);
     callback && callback();
   });
 
-
-
   socket.on('leave', (data) => {
     console.log("Leaved")
     socket.leave(data.room);
+  });
+
+  socket.on('disconnect', () => {
+    console.log("disconnected")
   });
 });
 
