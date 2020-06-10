ff203edcda8492cb2b0c7bc6ade4e6b2b1ba7757
Salwa M <codernist.s@gmail.com>
2020-06-10 20:33:12 +0500

    Post Privacy api

--- app/Models/UserPrivacy.php
+++ app/Models/UserPrivacy.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class UserPrivacy extends Model
 {
     protected $table = 'user_privacy';
+    protected $fillable = [ 'privacy_type_id', 'privacy_type', 'privacy_id', 'user_id' ];
 
     protected $hidden = [
         'privacy_type', 'created_at', 'updated_at'
