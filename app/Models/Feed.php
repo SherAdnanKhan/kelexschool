65b9a59ef0abd169a304876d3a119ca7223469f2
Salwa M <codernist.s@gmail.com>
2020-06-12 20:02:31 +0500

    Feed create

---
+++ app/Models/Feed.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Feed extends Model
+{
+    protected $fillable = [ 'feed', 'parent_id', 'created_by' ];
+
+    protected $hidden = [
+        'created_at', 'updated_at', 'deleted_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Feed::class,'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
+}
