a0a7008a21974ec017a90101e78b1fde9be004ee
Salwa M <codernist.s@gmail.com>
2020-06-16 21:07:54 +0500

    debugged faves

--- app/Http/Controllers/API/v1/PrivacyController.php
+++ app/Http/Controllers/API/v1/PrivacyController.php
@@ -37,14 +37,26 @@ class PrivacyController extends BaseController
     {
         $returnData = $user_list_ids = [];
         $my_user = Auth::guard('api')->user();
-        $user_lists = UserSprvfsIO::where([
-            ['status',  $status], 
-            ['privacy_type_id', $privacy_type_id],
-            ['created_to', $my_user->id] 
-            ])->get(); 
-        foreach($user_lists as $user_list) {
-            array_push($user_list_ids, $user_list->created_to);
+        if( $privacy_type_id == 3 ) {
+            $user_lists = UserSprvfsIO::where([
+                ['status',  $status], 
+                ['privacy_type_id', $privacy_type_id],
+                ['created_to', $my_user->id] 
+                ])->get(); 
+            foreach($user_lists as $user_list) {
+                array_push($user_list_ids, $user_list->created_by);
+            }
+        }else {
+            $user_lists = UserSprvfsIO::where([
+                ['status',  $status], 
+                ['privacy_type_id', $privacy_type_id],
+                ['created_to', $my_user->id] 
+                ])->get(); 
+            foreach($user_lists as $user_list) {
+                array_push($user_list_ids, $user_list->created_by);
+            }
         }
+        
         $returnData['faves'] = $all_faved_users = User::with('avatars', 'art.parent', 'galleries')->whereIn('id', $user_list_ids)->get();
         return $this->sendResponse($returnData, 'users lists');
         
