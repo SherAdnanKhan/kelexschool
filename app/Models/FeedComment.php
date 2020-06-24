d227c58e59b5b76c501128f8f6b1ed96dd4d75d8
Salwa M <codernist.s@gmail.com>
2020-06-24 18:08:27 +0500

    comment on feed

--- app/Models/FeedComment.php
+++ app/Models/FeedComment.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class FeedComment extends Model
 {
-    //
+    protected $fillable = [ 'feed_id', 'comment', 'created_by' ];
+
+    protected $hidden = [
+      'created_at', 'updated_at', 'deleted_at'
+    ];
+
+    public function user()
+    {
+      return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function feed()
+    {
+      return $this->belongsTo(Feed::class, 'feed_id');
+    }
 }
