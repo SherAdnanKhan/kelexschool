4c6192bb3ae53fab2ebfabbc906db7a437190be3
Salwa M <codernist.s@gmail.com>
2020-07-08 21:00:55 +0500

    node envs

--- socket.js
+++ socket.js
@@ -1,87 +1,61 @@
 const dotenv = require('dotenv');
 dotenv.config();
-var cors = require('cors');
 const express = require('express');
 const app = express();
-
-app.use(cors());
-let https, io, fs, path;
-
-if (`${process.env.APP_ENV}` == 'local') {
-  https = require('http').Server(app);
-  io = require('socket.io')(https);
-} else {
-  https = require('https');
+let server, io;
+if (`${process.env.NODE_APP_ENV}` == 'production') {
+  let fs, path;
   fs = require('fs');
   path = require('path');
-  io = require('socket.io')(https, { path: '/nodejsApp' });
+  const serverOptions = {
+    key: fs.readFileSync(path.join(__dirname, './certs/privkey.pem')),
+    cert: fs.readFileSync(path.join(__dirname, './certs/cert.pem'))
+  };
+  server = require('https').createServer(serverOptions, app);
+} else {
+  server = require('http').createServer(app);
 }
-
-
-let port = 8080;
-
+io = require('socket.io')(server);
 io.origins('*:*');
 io.sockets.on('connection', function (socket) {
   console.log('user connected');
-
   socket.on('join', (data, callback) => {
     socket.join(data.room);
     console.log(data.room);
     callback && callback();
   });
-
   socket.on('joinUser', (user, callback) => {
     socket.join(user.slug);
     console.log(user.slug);
     console.log('user join user');
     callback && callback();
   });
-
   socket.on('onRead', (data, callback) => {
     io.to(data.room).emit('read', data);
     callback && callback();
   });
-
   socket.on('onReadAll', (data, callback) => {
     io.to(data.room).emit('readAll', data);
     callback && callback();
   });
-
   socket.on('sendMessage', (data, callback) => {
     io.to(data.room).emit('recieveMessage', data);
     io.to(data.reciver).emit('notify', data);
     callback && callback();
   });
-
   socket.on('leave', (data) => {
     socket.leave(data.room);
   });
-
   socket.on('userLeft', (user) => {
     socket.joinUser(user.slug);
   });
-
   socket.on('userColorChange', (user) => {
     io.to(user.slug).emit('notifyColrChange', user);
   });
-
   socket.on('disconnect', () => {
     console.log('disconnected');
   });
 });
-
-if (`${process.env.APP_ENV}` == 'local') {
-  const server = https.listen(8080, function () {
-    console.log('listening on *:8080');
-  });
-} else {
-  const serverOptions = {
-    key: fs.readFileSync(path.join(__dirname, './certs/privkey.pem')),
-    cert: fs.readFileSync(path.join(__dirname, './certs/cert.pem'))
-  };
-  https.createServer(serverOptions, app).listen(port, () => {
-    console.log('Server is running on https://', port);
-  });
-
-}
-
+server.listen(`${process.env.NODE_APP_PORT}`, () => {
+  console.log('Server (', `${process.env.NODE_APP_ENV}`, ') is running on: ', `${process.env.NODE_APP_PORT}`);
+});
