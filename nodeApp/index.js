9d1a257257d1815a5f176dd3687a96a7ed323f88
Salwa M <codernist.s@gmail.com>
2020-07-13 14:21:03 +0500

    config changes port env

--- nodeApp/index.js
+++ nodeApp/index.js
@@ -1,11 +1,13 @@
-require('./config')();
+//require('./config')();
+const dotenv = require('dotenv');
+dotenv.config({ path: __dirname + '/../.env' });
+
 const express = require('express');
 const app = express();
 server = require('http').createServer(app);
 require('./socket')(server);
 
-let port = 8080;
-
-server.listen(port, () => {
-  console.log('Server is running on: ', port);
+console.log(process.env.NODE_APP_PORT);
+server.listen(process.env.NODE_APP_PORT, () => {
+  console.log('Server is running on: ', process.env.NODE_APP_PORT);
 });
