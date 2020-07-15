69751afbb774b22961de22573bfaad4376a6ba66
Salwa M <codernist.s@gmail.com>
2020-07-15 19:19:09 +0500

    url issue

--- nodeApp/requestServices.js
+++ nodeApp/requestServices.js
@@ -3,7 +3,7 @@ var base_url = process.env.BACKEND_APP_URL;
 
 module.exports = {
   sendMessage: function (data, token) {
-    return axios.post(base_url + 'chats/message', data, {
+    return axios.post(base_url + `chats/message`, data, {
       headers: {
         Authorization: `Bearer ${token}`,
         Accept: 'application/json'
