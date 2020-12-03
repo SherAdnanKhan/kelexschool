5989ca2b70542605f5bb57b21d53357adf82c40b
Salwa M <codernist.s@gmail.com>
2020-12-03 18:52:48 +0500

    dob fields added

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'first_name', 'last_name', 'username', 'email', 'password', 'slug', 'last_login'
+        'first_name', 'last_name', 'username', 'email', 'password', 'slug', 'last_login', 'dob'
     ];
 
     /**
