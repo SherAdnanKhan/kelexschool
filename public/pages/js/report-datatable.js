f435e61aa4deef6f20d7fa4052e93d85398fe931
Salwa M <codernist.s@gmail.com>
2020-12-31 11:26:55 +0500

    feedback-userreport- image error fixed

--- public/pages/js/report-datatable.js
+++ public/pages/js/report-datatable.js
@@ -60,12 +60,12 @@ var KTFeebackListDatatable = function () {
         title: 'Reported By',
         template: function (data) {
           console.log(data);
-          var avatars = data.report_by_user.avatar;
+          var avatars = data.report_by_user;
           var output = '';
-          if(avatars!=null){
+          if(avatars.avatar!=null){
             output = '<div class="kt-user-card-v2">\
                         <div class="kt-user-card-v2__pic">\
-                          <img src="'+ avatars.path + '" alt="photo">\
+                          <img src="'+ avatars.avatar.path + '" alt="photo">\
                         </div>\
                         <div class="kt-user-card-v2__details">\
                           <a href="'+ base_url + '/admin/user/' + data.report_by_user.slug + '" class="kt-user-card-v2__name">' + data.report_by_user.username + '</a>\
@@ -80,12 +80,12 @@ var KTFeebackListDatatable = function () {
         title: 'Reported Against',
         // callback function support for column rendering
         template: function (data) {
-          var avatars = data.report_to_user.avatar;
+          var avatars = data.report_to_user;
           var output = '';
-          if(avatars!=null){
+          if(avatars.avatar!=null){
             output = '<div class="kt-user-card-v2">\
                         <div class="kt-user-card-v2__pic">\
-                          <img src="'+ avatars.path + '" alt="photo">\
+                          <img src="'+ avatars.avatar.path + '" alt="photo">\
                         </div>\
                         <div class="kt-user-card-v2__details">\
                           <a href="'+ base_url + '/admin/user/' + data.report_to_user.slug + '" class="kt-user-card-v2__name">' + data.report_to_user.username + '</a>\
